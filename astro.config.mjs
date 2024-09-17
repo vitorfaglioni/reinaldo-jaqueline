88a94956e52d21b0f3fa92a57f362555b3803f40
vitorfaglioni@gmail.com <vitorfaglioni@gmail.com>
2024-09-17 17:30:27 -0300

    started cloning pelada to rabisco

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,6 +5,6 @@ import sitemap from '@astrojs/sitemap';
 
 // https://astro.build/config
 export default defineConfig({
-	site: 'https://peladananet.com.br',
+	site: 'https://rabiscofalado.com.br',
 	integrations: [mdx(), sitemap()]
 });
